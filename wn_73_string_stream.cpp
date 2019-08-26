fe91a35234ee44e3c462990e164291a64c3ccd8f
afewhee <afewhee@gmail.com>
2019-08-26 16:09:15 +0900

    [afew] string stream uuid

---
+++ wn_73_string_stream.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+
+std::string stream_to_string(const std::stringstream& src_stream)
+{
+	std::string ret = src_stream.str();
+	return ret;
+}
+
+std::stringstream string_to_stream(const std::string& src_str)
+{
+	return std::stringstream(src_str);
+}
+
+static int main()
+{
+	std::stringstream str_stream("1234567890123456789012345678901234567890");
+	std::string src_str = stream_to_string(str_stream);
+	long long deci_64 = atoll(src_str.c_str());
+	str_stream = string_to_stream("Hello world");
+	str_stream = string_to_stream(src_str);
+	str_stream >> deci_64;
+	return 0;
+}
---
+++ wn_73_uuid.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+
+#include <windows.h>
+#pragma comment(lib, "rpcrt4.lib")  // UuidCreate - Minimum supported OS Win 2000
+
+std::string cbi_uuid()
+{
+	std::string ret;
+	UUID uuid;
+	if(RPC_S_OK != ::UuidCreate(&uuid))
+		return {};
+	char* s;
+	::UuidToStringA(&uuid, (RPC_CSTR*)&s);
+	ret = (const char*)s;
+	::RpcStringFreeA((RPC_CSTR*)&s);
+	return ret;
+}
+
+int main()
+{
+	std::string uuid = cbi_uuid();
+	std::cout<<uuid<<std::endl;
+	return 0;
+}
